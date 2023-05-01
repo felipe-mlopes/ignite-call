43c3e021de5bd692e7a28c675d209a3136d61b1f
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-05-01 13:35:52 -0300

    firt commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    domains: ['https://lh3.googleusercontent.com/']
+  }
 }
 
 module.exports = nextConfig
